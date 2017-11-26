32383325f094bff6b8579b35b8318a38bda4dbf8
digitalvex <mr.zachary.harvey@gmail.com>
2017-11-25 21:09:25 -0700

    got opening local program a setting up a new project working.

--- main.py
+++ main.py
@@ -1,7 +1,44 @@
-# import pyautogui as pygui
+import pyautogui as pygui
+import time
 
-# test commit v1
-# select software
-choice = input("Are you using paint or paint.net?")
-x = 5
-# test commit
+choice = input('Enter name of your paint program')
+
+pygui.press('win')
+pygui.typewrite(choice)
+pygui.press('enter')
+
+paint_window = pygui.getWindow(choice)
+
+
+def resize_brush(size):
+    pygui.click(234, 101)
+    pygui.typewrite(size)
+    center_brush()
+    return True
+
+
+def center_brush():
+    cen_x = (float(pygui.size()[0])) * .5
+    cen_y = (float(pygui.size()[1])) * .5
+    pygui.moveTo(cen_x, cen_y)
+
+
+def create_new_project():
+    pygui.hotkey('ctrl', 'n')
+    # canvas width
+    pygui.typewrite('600')
+    pygui.press('tab')
+    # canvas height
+    pygui.typewrite('600')
+    pygui.press('enter')
+    return True
+
+
+# waiting for window to open, in order to maximize
+while not paint_window:
+    paint_window = pygui.getWindow(choice)
+    time.sleep(1)
+else:
+    pygui.getWindow(choice).maximize()
+
+create_new_project()
